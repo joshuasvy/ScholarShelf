b4fb2f42bcb432cca5f6fd0a0a9d220e5876000a
joshuasvy <joshuapaulcortez@gmail.com>
2026-02-07 21:36:35 +0800

    adding users model

---
+++ backend/src/models/users.ts
@@ -0,0 +1,15 @@
+import connection from "../config/connection.js";
+
+async function insertUsers(
+  name: string,
+  email: string,
+  contact: string,
+  address: string,
+  password: string,
+) {
+  const query = `INSERT INTO users (name, email, contact, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
+
+  const values = [name, email, contact, address, password];
+  const result = await connection.query(query, values);
+  return result.rows[0];
+}
