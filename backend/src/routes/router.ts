6120b6df6423ce8886b42a7e45279a10b9350bc9
joshuasvy <joshuapaulcortez@gmail.com>
2026-02-13 23:39:27 +0800

    new url path for book (api/books/...)

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import users from "../controller/users.controller.js";
+import books from "../controller/books.controller.js";
 
 const router = Router();
 
 router.use("/api/auth", users);
+router.use("/api/books", books);
 
 export default router;
