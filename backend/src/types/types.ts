f30eb781377a3ba26c1b6c373bbd80b1109b06e1
joshuasvy <joshuapaulcortez@gmail.com>
2026-02-14 16:04:15 +0800

    improving model to return strongly typed object

---
+++ backend/src/types/types.ts
@@ -0,0 +1,17 @@
+export interface Book {
+  id: number;
+  book_cover: string;
+  title: string;
+  sub_title?: string;
+  author: string;
+  language: string;
+  abstract: string;
+  publisher: string;
+  year: number;
+  citation?: string;
+  topic: string;
+  shelf_code: number;
+  status: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
