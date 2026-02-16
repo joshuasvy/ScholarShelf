89c596d445e9c32c82f0c436639091fe0be451db
joshuasvy <joshuapaulcortez@gmail.com>
2026-02-16 13:59:29 +0800

    fetching data from books data using BookCardProps

--- frontend/src/assets/components/BooksCard.tsx
+++ frontend/src/assets/components/BooksCard.tsx
@@ -1,41 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import type { BookInterface } from "../../types/type";
 import StatusBadge from "./StatusBadge";
 
-interface BooksProps {
-  id: number;
-  cover: string;
-  title: string;
-  author: string;
-  year: number;
-  status?: string;
+interface BookCardProps {
+  book: BookInterface;
 }
 
-export default function BooksCard({
-  cover,
-  title,
-  author,
-  year,
-  status,
-}: BooksProps) {
+export default function BooksCard({ book }: BookCardProps) {
   const navigate = useNavigate();
 
   return (
     <div className="w-30 md:w-42 h-fit flex flex-col gap-2 cursor-pointer">
       <button
-        onClick={() => navigate("/book/details")}
+        onClick={() => navigate(`book/${book.id}`)}
         className="flex flex-col gap-2 cursor-pointer transition-transform duration-300 hover:scale-103"
       >
         <img
-          src={cover}
-          alt={cover}
+          src={book.book_cover}
+          alt={book.title}
           className="w-full rounded-md shadow-lg object-cover"
         />
         <div className="flex flex-col gap-2 font-inter text-left">
-          <p className="font-medium text-md line-clamp-2">{title}</p>
-          <p className="text-sm line-clamp-1">{author}</p>
-          <p className="text-sm">{year}</p>
+          <p className="font-medium text-md line-clamp-2">{book.title}</p>
+          <p className="text-sm line-clamp-1">{book.author}</p>
+          <p className="text-sm">{book.year}</p>
           <StatusBadge
-            status={status as "Pending" | "Approved" | "Rejected" | "Completed"}
+            status={
+              book.status as "Pending" | "Approved" | "Rejected" | "Completed"
+            }
           />
         </div>
       </button>
