5f57525aa34273c0cafeffb787425c4218f8543b
joshuasvy <joshuapaulcortez@gmail.com>
2026-02-16 13:58:06 +0800

    mapping the book.id or books data from useBooks hook

--- frontend/src/assets/pages/Books.tsx
+++ frontend/src/assets/pages/Books.tsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
-import { books } from "../../../data/books";
+// import { useState } from "react";
+import { useBooks } from "../../hooks/useBooks";
+import type { BookInterface } from "../../types/type";
 import Header from "../components/Header";
 import Breadcrumb from "../components/Breadcrumb";
 import BooksCard from "../components/BooksCard";
-import Pagination from "../components/Pagination";
+// import Pagination from "../components/Pagination";
 import Footer from "../components/Footer";
 
 function Books() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 21;
+  const { books, loading, error } = useBooks();
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const itemsPerPage = 21;
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentBooks = books.slice(indexOfFirstItem, indexOfLastItem);
+  // const indexOfLastItem = currentPage * itemsPerPage;
+  // const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  // const currentBooks = books.slice(indexOfFirstItem, indexOfLastItem);
+
+  if (loading) return <p>Loading Books...</p>;
+  if (error) return <p style={{ color: "red" }}>Error: {error}</p>;
 
   return (
     <div className="bg-primary min-h-screen w-full">
@@ -42,24 +47,18 @@ function Books() {
           </div>
         </div>
         <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-7 gap-2 mt-12 space-y-3 justify-items-center px-4 md:px-12 lg:px-0">
-          {currentBooks.map((book) => (
-            <BooksCard
-              id={book.id}
-              cover={book.cover}
-              title={book.title}
-              author={book.author}
-              year={book.year}
-            />
+          {books.map((book: BookInterface) => (
+            <BooksCard key={book.id} book={book} />
           ))}
         </div>
-        <div>
+        {/* <div>
           <Pagination
             totalBooks={books.length}
             items={itemsPerPage}
             currentPage={currentPage}
             onPageChange={setCurrentPage}
           />
-        </div>
+        </div> */}
       </main>
       <div>
         <Footer />
