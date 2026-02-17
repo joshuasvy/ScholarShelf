de59e9793eba7dc15fd9c4f6012e376f90f715fc
joshuasvy <joshuapaulcortez@gmail.com>
2026-02-17 15:16:12 +0800

    adding a dynamic file path for topics with encodeURIComponent for readability

--- frontend/src/assets/components/Categories.tsx
+++ frontend/src/assets/components/Categories.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Categories() {
+  const navigate = useNavigate();
+
   const topics = [
     { name: "Antropology", icon: "/images/icons/antropology.png" },
     { name: "Architecture", icon: "/images/icons/architecture.png" },
@@ -19,9 +23,14 @@ export default function Categories() {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-2 lg:gap-4 justify-items-center lg:px-0">
       {topics.map((topic) => (
-        <button className="w-50 h-12 md:54 lg:w-58 lg:h-14 flex flex-row justify-center items-center gap-3 rounded-md shadow-md cursor-pointer hover:bg-secondary transition-colors duration-100">
+        <button
+          onClick={() => navigate(`/catalog/${encodeURIComponent(topic.name)}`)}
+          className="w-50 h-12 md:54 lg:w-58 lg:h-14 flex flex-row justify-center items-center gap-3 rounded-md shadow-md cursor-pointer hover:bg-secondary transition-colors duration-100"
+        >
           <img src={topic.icon} alt={topic.icon} className="w-5 md:w-6" />
-          <span className="font-inter font-semibold text-md md:text-lg">{topic.name}</span>
+          <span className="font-inter font-semibold text-md md:text-lg">
+            {topic.name}
+          </span>
         </button>
       ))}
     </div>
