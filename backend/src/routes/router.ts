9e2e5e9e660fec42b51a547dfede1571b216d31d
joshuasvy <joshuapaulcortez@gmail.com>
2026-02-21 16:36:37 +0800

    adding reservation route

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import users from "../controller/users.controller.js";
 import books from "../controller/books.controller.js";
+import reservations from "../controller/reservations.controller.js";
 
 const router = Router();
 
 router.use("/api/auth", users);
 router.use("/api/books", books);
+router.use("/api/reservations", reservations);
 
 export default router;
